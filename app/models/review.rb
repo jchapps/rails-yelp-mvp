7c29d9654438125ed8da774f90925cb455418653
Josh Chappelow <jdchappelow@gmail.com>
2022-08-10 18:33:08 +0900

    green

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,4 @@ class Review < ApplicationRecord
   validates :content, presence: true
   validates :rating, numericality: { only_integer: true }
   validates :rating, :inclusion => 0..5
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,18 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
+Restaurant.destroy_all
+puts "Cleaning the DB..."
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-5.times do
+20.times do
   restaurant = Restaurant.new(
     name: Faker::Games::ElderScrolls.name,
     address: Faker::Games::ElderScrolls.city,
-    category: Restaurant::CATEGORIES.sample
+    category: Restaurant::CATEGORIES.sample,
+    phone_number: Faker::PhoneNumber.cell_phone
   )
   restaurant.save
 end
